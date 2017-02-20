c5858ef21876ba21d5b86b4b2f341e0b1e225dd1
Aurelis-Loire <johanna.oberto@gmail.com>
2017-02-20 11:56:08 -0500

    Adding profile page and links between profile and home pages

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,10 @@
 class ProfileController < ApplicationController
   def index
+    @firstname = 'Jane'
+    @lastname = 'Doe'
+    @email = 'janedoe@email.com'
+    @pass = 'pass'
+    @money = '$60'
   end
 
   def create
