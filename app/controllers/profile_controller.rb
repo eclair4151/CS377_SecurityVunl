f62fbb3dea5b4c6a0abfb106ede13ae8cd0f2cf2
Tomer Shemesh <tomerjshemesh@gmail.com>
2017-02-21 15:02:17 -0500

    show profile page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,11 +1,7 @@
 class ProfileController < ApplicationController
   layout 'accountManagement'
   def index
-    @firstname = 'Jane'
-    @lastname = 'Doe'
-    @email = 'janedoe@email.com'
-    @pass = 'pass'
-    @money = '$60'
+    @user = Users.find_by_session_id(cookies[:session_id])
   end
 
   def create
