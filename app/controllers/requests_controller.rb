5fc05129e8e41db63f43f36d8363f54560c5e2bb
Tomer Shemesh <tomerjshemesh@gmail.com>
2017-02-27 11:41:11 -0500

    approve and deny requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -9,5 +9,26 @@ class RequestsController < ApplicationController
   end
 
   def create
+    user =  User.find_by_session_id(cookies[:session_id])
+    request = user.received_requests.find_by(id: params[:request_id])
+    if(params[:type] = 'Send')
+        if user.money > request.amount
+          sender = request.sender
+          sender.money += request.amount
+          user.money -= request.amount
+          user.sent_transactions.create(recipient_id: sender.id, amount: request.amount,message: request.message)
+          user.save
+          sender.save
+          Request.destroy(request.id)
+          redirect_to '/requests', :flash => { :message => "Funds Successfully Sent" }
+        else
+          redirect_to '/requests', :flash => { :error => "Insufficient Funds" }
+        end
+    else
+      Request.destroy(request.id)
+      redirect_to '/requests', :flash => { :message => "Funds Successfully Denied" }
+    end
+
+
   end
 end
