fe890d86435c617278d567bdc312882f9c2e4844
microlm <laura.mo@gmail.com>
2017-03-01 10:56:53 -0500

    Added caesar shift to encrypt password upon submitting form

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,2 +1,34 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var shiftNumber = 5;
+
+function DoLogin()
+{
+    document.loginForm.password.value = caesarShift(document.loginForm.password.value, shiftNumber);
+    return true;
+}
+
+function caesarShift(text, shift)
+{
+    var result = "";
+
+    for(var i = 0; i < text.length; ++i)
+    {
+        var c = text.charCodeAt(i);
+        if (c >= 65 && c <= 90)
+        {
+            result += String.fromCharCode((c - 65 + shift) % 26 + 65);
+        }
+        else if(c >= 97 && c <= 122)
+        {
+            result += String.fromCharCode((c - 97 + shift) % 26 + 97);
+        }
+        else
+        {
+            result += String.fromCharCode(c + shift);
+        }
+    }
+
+    return result;
+}
